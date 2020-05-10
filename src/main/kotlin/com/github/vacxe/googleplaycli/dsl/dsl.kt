de1cb10fb96536b65b1fef02084de03f8f1df171
Vacxe <aksenov.kostya@gmail.com>
2020-05-09 21:33:27 -0700

    Review fixes

--- src/main/kotlin/com/github/vacxe/googleplaycli/dsl/dsl.kt
+++ src/main/kotlin/com/github/vacxe/googleplaycli/dsl/dsl.kt
@@ -6,19 +6,13 @@ import com.github.vacxe.googleplaycli.PlayStoreCli
 import com.github.vacxe.googleplaycli.core.BaseCommand
 import com.github.vacxe.googleplaycli.core.MetaCommand
 
-fun cmd(name: String, configurationBlock: MetaCommand.() -> Unit): MetaCommand {
-    val metaCommand = MetaCommand(name)
-    metaCommand.configurationBlock()
-    return metaCommand
-}
+fun cmd(name: String, configurationBlock: MetaCommand.() -> Unit): MetaCommand = MetaCommand(name).apply(configurationBlock)
 
 fun MetaCommand.subcmd(name: String, configurationBlock: MetaCommand.() -> Unit) {
-    val metaCommand = MetaCommand(name)
-    metaCommand.configurationBlock()
-    subcommands(metaCommand)
+    subcommands(cmd(name, configurationBlock))
 }
 
-fun MetaCommand.baseCmd(name: String, description: String = "", block: BaseCommand.(PlayStoreCli) -> Unit) {
+fun MetaCommand.baseCmd(name: String, description: String = "", block: BaseCommand.(PlayStoreCli) -> Any) {
     subcommands(
             object : BaseCommand(name = name, actionDescription = description) {
                 override fun run(cli: PlayStoreCli) = block(cli)
