ef0b054a049293c62722f82e725ea3ab1d7b98cf
Vacxe <aksenov.kostya@gmail.com>
2020-05-13 01:04:48 -0700

    Remove unused dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,13 +1,11 @@
 object Versions {
     const val clikt = "2.6.0"
-    const val googleApiClient = "1.30.9"
     const val googleOAuth = "0.4.0"
     const val googleAppPublisher = "v3-rev103-1.25.0"
 }
 
 object Libraries {
     const val clikt = "com.github.ajalt:clikt:${Versions.clikt}"
-    const val googleApiClient = "com.google.api-client:google-api-client:${Versions.googleApiClient}"
     const val googleOAuth = "com.google.auth:google-auth-library-oauth2-http:${Versions.googleOAuth}"
     const val googleAppPublisher = "com.google.apis:google-api-services-androidpublisher:${Versions.googleAppPublisher}"
 }
